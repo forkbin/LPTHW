ce25f8459727464758126771dc804fb075e0787f
forkbin <mwguo07@163.com>
2015-12-04 00:26:14 +0800

    exercise 17-33

---
+++ ex22.py
@@ -0,0 +1,26 @@
+# --coding: utf-8--
+
+# 01 print
+# 02 "#" comments
+# 03 math symbols
+# 04 variables, names
+# 05 format string
+# 06 string adding
+# 07 string * number
+# 08 %r. The %r will give you the "raw programmer's" version of variable, also known as the "representation."
+# 09 """  '''
+# 10 Escape Sequences
+# 11 raw_input(prompt)
+# 12 more prompting
+# 13 import, unpacking
+# 14 prompt = ">"
+# 15 open, read
+# 16 open, truncate, write, close
+# 17 copy file content
+# 18 functions
+# 19 functions and variables
+# 20 read, seek, readline
+# 21 returns
+
+
+
